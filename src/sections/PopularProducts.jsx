882e80b485a0421f0bff74c8545112344a7c2c34
Muhammad Aliyu Musa <muhammadhadiali485@gmail.com>
2024-05-18 12:53:35 +0100

    finished hero section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,15 @@
+import { products } from "../constants"
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id="product" className=" max-container max-sm:mt-12 ">
+      <div className="flex flex-col justify-center gap-5">
+        <h2 className=" text-4xl font-palanquin font-bold">Our <span className=" text-coral-red">Popular</span> Products</h2>
+        <p>
+          Experience the top-notch quality and style with our sougth after selections. discover a world of comfort, design, and value.
+        </p>
+      </div>
+
+    </section>
   )
 }
 
